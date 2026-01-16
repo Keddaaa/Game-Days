d2ce1bb74b9ba5eb322d8926a72538b47ad87c27
KEDDAA <130056507+Keddaaa@users.noreply.github.com>
2026-01-16 12:22:58 +0100

    d

---
+++ Game-days/src/components/navbar/navbar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./navbar.scss";
+
+const Navbar = () => {
+	return (
+		<nav>
+			<p>Navbar</p>
+		</nav>
+	);
+};
+
+export default Navbar;
---
+++ Game-days/src/pages/index/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.scss";
+
+const Index = () => {
+	return (
+		<div>
+			<h1>Index</h1>
+		</div>
+	);
+};
+
+export default Index;
