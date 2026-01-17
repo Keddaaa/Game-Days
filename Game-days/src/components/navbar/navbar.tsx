696afd54e631b62b16875983d3c9aaf0cf84badb
KEDDAA <130056507+Keddaaa@users.noreply.github.com>
2026-01-17 01:33:42 +0100

    nav

--- Game-days/src/components/navbar/navbar.tsx
+++ Game-days/src/components/navbar/navbar.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
 	return (
 		<nav>
-			<p>Navbar</p>
+			<ul>
+				<li>
+					<Link to="">Accueil</Link>
+				</li>
+				<li>
+					<Link to="">L’événement</Link>
+				</li>
+				<li>
+					<Link to="">Vote tes jeux</Link>
+				</li>
+				<li>
+					<Link to="">Contact</Link>
+				</li>
+			</ul>
 		</nav>
 	);
 };
