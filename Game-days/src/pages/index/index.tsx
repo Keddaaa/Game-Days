6589daa8ae17cfaf1cda4563c611cbe1169412e3
KEDDAA <130056507+Keddaaa@users.noreply.github.com>
2026-01-19 00:02:32 +0100

    oui

--- Game-days/src/pages/index/index.tsx
+++ Game-days/src/pages/index/index.tsx
@@ -1,11 +1,19 @@
-import React from "react";
 import "./index.scss";
 
 const Index = () => {
 	return (
-		<div>
-			<h1>Index</h1>
-		</div>
+		<section className="index">
+			<div className="hero">
+				<img src="../../img/valorant.gif" alt="Fond d'écran Valorant" />
+				<div className="hero-content">
+					<h1>MMI Game Days</h1>
+					<p>La première édition gaming de l’IUT de Meaux</p>
+					<button className="voirplus">
+						<img src="../../icons/arrow.svg" alt="Voir plus" />
+					</button>
+				</div>
+			</div>
+		</section>
 	);
 };
 
