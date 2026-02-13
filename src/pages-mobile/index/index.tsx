9d89e1f82868fcf751a2eddd51bc4128cd78cd63
goola971 <janviercharly97114@gmail.com>
2026-02-13 16:02:01 +0100

    save

--- src/pages-mobile/index/index.tsx
+++ src/pages-mobile/index/index.tsx
@@ -1,6 +1,34 @@
 import "./index.scss";
+import { useEffect, useState } from "react";
 
 const Index = () => {
+    // compte a rebours a partir d'une date precise
+    // 23 mars 2026 16h
+    const date = new Date("2026-03-23T16:00:00");
+    const [days, setDays] = useState(0);
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+
+    // chaque seconde le compte a rebours diminue
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const now = new Date();
+            const diff = date.getTime() - now.getTime();
+            const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+            const hours = Math.floor(
+                (diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+            );
+            const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+            setDays(days);
+            setHours(hours);
+            setMinutes(minutes);
+            setSeconds(seconds);
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <div className="indexMobile">
             <section className="hero">
@@ -21,23 +49,23 @@ const Index = () => {
                     <h2>C’est quand ?</h2>
                     <div className="grid">
                         <div className="card">
-                            <h4>37</h4>
+                            <h4>{days}</h4>
                             <p>jours</p>
                         </div>
                         <div className="card">
-                            <h4>18</h4>
+                            <h4>{hours}</h4>
                             <p>heures</p>
                         </div>
                         <div className="card">
-                            <h4>16</h4>
+                            <h4>{minutes}</h4>
                             <p>minutes</p>
                         </div>
                         <div className="card">
-                            <h4>23</h4>
+                            <h4>{seconds}</h4>
                             <p>secondes</p>
                         </div>
-                        <hr className="line" />
-                        <hr className="line" />
+                        <hr className="hr1" />
+                        <hr className="hr2" />
                     </div>
                 </div>
             </section>
