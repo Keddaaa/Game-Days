a0da7c64bbebec3d112d7a0a1914484b31da20b8
goola971 <janviercharly97114@gmail.com>
2026-03-15 23:39:39 +0100

    save

--- src/pages/votes/finvote.tsx
+++ src/pages/votes/finvote.tsx
@@ -1,28 +1,28 @@
 import { useNavigate } from "react-router-dom";
-import "../styles/vote1.scss";
+import "./vote1.scss";
 
 const Vote8 = () => {
-	const navigate = useNavigate();
+    const navigate = useNavigate();
 
-	return (
-		<div className="vote-page">
-			<main className="content">
-				<h1>Merci pour tes réponses !</h1>
+    return (
+        <div className="vote-page">
+            <main className="content">
+                <h1>Merci pour tes réponses !</h1>
 
-				<div className="vote-success">
-					Ton vote a bien été pris en compte
-				</div>
+                <div className="vote-success">
+                    Ton vote a bien été pris en compte
+                </div>
 
-				<button
-					className="next-button"
-					onClick={() => navigate("/")}
-					style={{ marginTop: "20px" }}
-				>
-					Retour
-				</button>
-			</main>
-		</div>
-	);
+                <button
+                    className="next-button"
+                    onClick={() => navigate("/")}
+                    style={{ marginTop: "20px" }}
+                >
+                    Retour
+                </button>
+            </main>
+        </div>
+    );
 };
 
 export default Vote8;
